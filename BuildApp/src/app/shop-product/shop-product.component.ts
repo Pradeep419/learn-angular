ec8abe5ec9e6e9fa09289e29feeeef128805a7b4
Pradeep419 <g.pradeep419@gmail.com>
2024-02-28 09:02:46 +0530

    Initial Commit of the angular app

---
+++ BuildApp/src/app/shop-product/shop-product.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shop-product',
+  standalone: true,
+  imports: [],
+  templateUrl: './shop-product.component.html',
+  styleUrl: './shop-product.component.css'
+})
+export class ShopProductComponent {
+
+}
